8c1cfed07f90c1679ce8e9a0da34b89027ad20be
github mytty870 <otani11ace@icloud.com>
2023-10-19 20:51:11 +0900

    add multiple columus

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import {DndContext, KeyboardSensor, PointerSensor, closestCenter, useSensor, useSensors} from '@dnd-kit/core';
-import { useState } from 'react';
-import { SortableContext, arrayMove, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable';
-import {SortableItem} from './SortableItem'
-import { x } from '@xstyled/styled-components';
+import { DndContext, KeyboardSensor, PointerSensor, closestCenter, useSensor, useSensors } from '@dnd-kit/core'
+import { useState } from 'react'
+import { SortableContext, arrayMove, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable'
+import { SortableItem } from './SortableItem'
+import { x } from '@xstyled/styled-components'
 
 function App() {
   const [items, setItems] = useState([
@@ -25,38 +25,59 @@ function App() {
     useSensor(KeyboardSensor, {
       coordinateGetter: sortableKeyboardCoordinates,
     })
-  );
+  )
 
   return (
-    <DndContext 
-      sensors={sensors}
-      collisionDetection={closestCenter}
-      onDragEnd={handleDragEnd}
-    >
-      <SortableContext items={items} strategy={verticalListSortingStrategy}>
-        <x.div background="#3b3b3b" w="30%" color="#fff" fontSize="0.8rem" textAlign="center" borderRadius="6px" p="10px 0 10px 0">
-          <x.h1>打順</x.h1>
-          {items.map(item => <SortableItem key={item.id} id={item.id} title={item.title} />)}
-        </x.div>
-      </SortableContext>
-
+    <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+      <x.div display="flex">
+        <SortableContext items={items} strategy={verticalListSortingStrategy}>
+          <x.div
+            background="#3b3b3b"
+            w="30%"
+            color="#fff"
+            fontSize="0.8rem"
+            textAlign="center"
+            borderRadius="6px"
+            p="10px 0 10px 0"
+            mr="50px"
+          >
+            <x.h1>打順</x.h1>
+            {items.map(item => (
+              <SortableItem key={item.id} id={item.id} title={item.title} />
+            ))}
+          </x.div>
+        </SortableContext>
+        <SortableContext items={items} strategy={verticalListSortingStrategy}>
+          <x.div
+            background="#3b3b3b"
+            w="30%"
+            color="#fff"
+            fontSize="0.8rem"
+            textAlign="center"
+            borderRadius="6px"
+            p="10px 0 10px 0"
+          >
+            <x.h1>打順</x.h1>
+            {items.map(item => (
+              <SortableItem key={item.id} id={item.id} title={item.title} />
+            ))}
+          </x.div>
+        </SortableContext>
+      </x.div>
     </DndContext>
   )
 
   function handleDragEnd(event: any) {
-    const {active, over} = event;
+    const { active, over } = event
     console.log(active)
-    
+
     if (active.id !== over.id) {
-      const oldIndex = items.findIndex((item) => item.id === active.id)
-          const newIndex = items.findIndex((item) => item.id === over.id)
-          const newItems = arrayMove(items, oldIndex, newIndex)
-          setItems(newItems)
+      const oldIndex = items.findIndex(item => item.id === active.id)
+      const newIndex = items.findIndex(item => item.id === over.id)
+      const newItems = arrayMove(items, oldIndex, newIndex)
+      setItems(newItems)
     }
   }
-
 }
 
-
-
-export default App;
+export default App
