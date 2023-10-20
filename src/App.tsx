502589404f5cfabf226c49fff1eeb0e11ab2be6d
github mytty870 <otani11ace@icloud.com>
2023-10-20 20:17:31 +0900

    add modifiers

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { useState } from 'react'
 import { SortableContext, arrayMove, sortableKeyboardCoordinates, verticalListSortingStrategy } from '@dnd-kit/sortable'
 import { SortableItem } from './SortableItem'
 import { x } from '@xstyled/styled-components'
+import { restrictToVerticalAxis, restrictToWindowEdges } from '@dnd-kit/modifiers'
 
 function App() {
   const [items, setItems] = useState([
@@ -28,7 +29,19 @@ function App() {
   )
 
   return (
-    <DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
+   <DndContext
+      sensors={sensors}
+      // colllisionDetection は基本 closestCenter を設定(defaultとして推奨)
+      collisionDetection={closestCenter}
+      // modifiers の設定
+      // restrictToVerticalAxis : 垂直方向のみの移動制限
+      // restrictToHorizontalAxis : 水平方向のみの移動制限
+      // restrictToWindowEdges : ウィンドウ内の移動のみに制限(ウインドウの中ならどこでも移動できる)
+      // restrictToParentElement : 親要素の内部の移動のみに制限
+      // restrictToFirstScrollableAncestor : 初回のスクロール可能な部分のみの移動に制限(試していないので、どのような挙動になるか不明)
+      modifiers={[restrictToVerticalAxis, restrictToWindowEdges]}
+      onDragEnd={handleDragEnd}
+    >
       <x.div display="flex">
         <SortableContext items={items} strategy={verticalListSortingStrategy}>
           <x.div
